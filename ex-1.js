b4fbdd532c56064a96d2939a7144349a4712fa8c
Napat <napat.wongchr@gmail.com>
2022-03-05 20:33:59 +0700

    feat: add ex

---
+++ ex-1.js
@@ -0,0 +1,8 @@
+let accountBalance = 400;
+let depositAmountFromInput = "10";
+
+accountBalance = accountBalance + depositAmountFromInput;
+
+let displayBalanceMessage = `Your account balance is ${accountBalance}`;
+
+console.log(displayBalanceMessage);

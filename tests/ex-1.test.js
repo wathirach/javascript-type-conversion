79aff51cb7ecbfbaf66ad13826620d3425641ffb
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-01-02 23:13:31 +0700

    fix:rename test case

--- tests/ex-1.test.js
+++ tests/ex-1.test.js
@@ -1,8 +1,23 @@
 // import displayAccountBalance from "../ex-1";
 import fs from "fs/promises";
 
-describe("Exercise 1", () => {
-  it("Balance account message should display correctly", async () => {
+describe("Exercise 1 : Type Conversion", () => {
+  test("Exercise 1 : ต้องมีการทำ Type Conversion", async () => {
+    const data = await fs.readFile("./ex-1.js");
+    let accountBalance = 400;
+    let depositAmountFromUser = "10";
+
+    const code = `${data}
+    return accountBalance;
+      `;
+
+    const func = new Function(code);
+    const result = func();
+
+    expect(result).toBe(accountBalance + Number(depositAmountFromUser));
+  });
+
+  test('Exercise 1 : accountBalanceMessage จะต้องมีค่าเป็น "Your account balance is 410"', async () => {
     const data = await fs.readFile("./ex-1.js");
 
     const code = `${data}

7dd0ccbed29b6dfb63c5ae4a79feff26627d1b52
Athirach <wathirach@gmail.com>
2024-08-20 21:11:41 +0700

    bp5-Wan: Debug Ex-1 type conversion

--- ex-1.js
+++ ex-1.js
@@ -1,6 +1,6 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
-
+depositAmountFromUser = Number(depositAmountFromUser)
 accountBalance = accountBalance + depositAmountFromUser;
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
